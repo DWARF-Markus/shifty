173d2b8fc72b7b514f605b0db6cbadfc1e18a958
DWARF-Markus <mbh@dwarf.dk>
2020-11-20 17:00:37 +0100

    implemented Next API routes setup prisma to connect to a postqres database

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,48 @@
 import Layout from '../components/Layout';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+async function createUserRequest() {
+  const obj = {
+    user: {
+      name: "Finally",
+      email: "it@works.dk",
+      password: "secret13123"
+    }
+  }
+
+  await fetch('/api/user', {
+    method: 'POST',
+    headers: {
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify(obj)
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  });
+}
+
+
+async function fetchUserData() {
+  const res = await fetch('/api/user');
+  const data = await res.json();
+  const { users } = data;
+  return users;
+}
+
 
 export default function HomePage() {
+
+  const {data: users} = useQuery('user', fetchUserData);
+
   return (
     <Layout>
       <h1>Home</h1>
+      <h1>{ users && users[0].email }</h1>
+
+      <button onClick={() => createUserRequest()}>Click me to add a new user</button>
     </Layout>
   )
 }
