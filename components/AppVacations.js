33838123604b7c1c3eec760a94bcf34edf8858e4
DWARF-Markus <mbh@dwarf.dk>
2020-12-12 22:11:47 +0100

    form setup for users

--- components/AppVacations.js
+++ components/AppVacations.js
@@ -1,12 +1,48 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import { useSelector } from 'react-redux';
+import InputField from './InputField';
 
 export default function AppVacations() {
+
+  const GET_STATE = useSelector((state) => state);
+
+  const handleVacationSubmit = async () => {
+    console.log('triggered')
+  }
+
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-      <div>
-        <h1>Vacations</h1>
-      </div>
+      <VacationsWrapper>
+        <VacationsTop>
+          <h3>{GET_STATE.isAdmin ? 'Vacations' : 'Register your vacation'}</h3>
+          {GET_STATE.isAdmin ? <></> : (
+            <>
+              <InputField type="date" label="Start date" getter="vacationStart" setter="SET_VACATION_START" />
+              <InputField type="date" label="End date" getter="vacationEnd" setter="SET_VACATION_END" />
+              <button onClick={() => handleVacationSubmit()} className="btn--primary">Submit</button>
+            </>
+          )}
+        </VacationsTop>
+        <VacationOverview>
+          {GET_STATE.isAdmin ? <></> : (
+            <>
+              <p>Overview</p>
+            </>
+          )}
+        </VacationOverview>
+      </VacationsWrapper>
     </motion.div>
   )
 }
+
+const VacationsWrapper = styled.div``;
+
+const VacationsTop = styled.div`
+  h3 {
+    font-weight: 300;
+    line-height: 1.2;
+  }
+`;
+
+const VacationOverview = styled.div``;
